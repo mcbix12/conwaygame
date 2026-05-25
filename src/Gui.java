a2494a99541498889c36442f7a7b1bc6911f952a
bix <henrybradley2009@gmail.com>
2026-05-25 13:40:00 +1200

    Begun on GUI

---
+++ src/Gui.java
@@ -0,0 +1,29 @@
+import javax.management.remote.JMXConnectionNotification;
+import javax.swing.*;
+import java.awt.*;
+public class Gui extends JFrame {
+JMenuBar menuBar;
+JMenu menu;
+JMenuItem menuItemNew;
+    public Gui() {
+    setTitle("Conways Game Of Life");
+
+    this.getContentPane().setPreferredSize(new Dimension(400,600));
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+    menuBar = new JMenuBar();
+    this.setJMenuBar(menuBar);
+
+    menu = new JMenu("Game");
+    menuBar.add(menu);
+
+    menuItemNew = new JMenuItem("new game");
+    menu.add(menuItemNew);
+
+    this.pack();
+    this.toFront();
+    this.pack();
+    this.setVisible(true);
+}
+}
