42955adfb5213b3b5fb2ba2c95cc82d29839e25d
bix <henrybradley2009@gmail.com>
2026-06-11 12:17:37 +1200

    It click now colour!

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,35 +1,50 @@
-import javax.swing.*;
-import java.awt.*;
+    import javax.swing.*;
+    import java.awt.*;
 
-public class GamePanel extends JPanel {
-    int cellSize = 20;
-    int cols = 60;
-    int rows = 35;
-    boolean[][] gridCells = new boolean[rows][cols];
-    public GamePanel() {
-       // gridCells[2][10] = true;
-    }
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (gridCells[row][col]) {
-                    //g.setColor(Color.green);
-                    //g.drawRect(col * cellSize, row * cellSize, cellSize, cellSize);
-                   // g.fillRect(col * cellSize + 2, row * cellSize + 2, cellSize - 4, cellSize - 4);
-                } else {
-                    g.setColor(Color.gray);
-                    g.drawRect(col * cellSize, row * cellSize, cellSize, cellSize);
-                    g.fillRect(col * cellSize + 2, row * cellSize + 2, cellSize - 4, cellSize - 4);
+    public class GamePanel extends JPanel {
+        public static boolean running = true;
+        int cellSize = 20;
+        static int cols = 60;
+        static int rows = 35;
+        public static int cordsx;
+        public static int cordsy;
+        public static boolean[][] gridCells = new boolean[rows][cols];
 
 
+        public GamePanel() {
 
-                    }
-
-
-
-
-                }
+        }
+        public void setCell(boolean[][] gridCells) {
+            if (gridCells[cordsy][cordsx] == false) {
+                gridCells[cordsy][cordsx] = true;
+                GamePanel.gridCells = gridCells;
+                repaint();
+            } else {
+                gridCells[cordsy][cordsx] = false;
+                GamePanel.gridCells = gridCells;
+                repaint();
             }
         }
-    }
+        public void paintComponent(Graphics g) {
+
+                super.paintComponent(g);
+
+                for (int row = 0; row < rows; row++) {
+                    for (int col = 0; col < cols; col++) {
+                        if (gridCells[row][col]) {
+                            g.setColor(Color.black);
+                            g.drawRect(col * cellSize, row * cellSize, cellSize, cellSize);
+                            g.fillRect(col * cellSize + 2, row * cellSize + 2, cellSize - 4, cellSize - 4);
+                        } else {
+                            g.setColor(Color.gray);
+                            g.drawRect(col * cellSize, row * cellSize, cellSize, cellSize);
+                            g.fillRect(col * cellSize + 2, row * cellSize + 2, cellSize - 4, cellSize - 4);
+
+
+                        }
+
+                    }
+                }
+            }
+
+        }
--- src/Gui.java
+++ src/Gui.java
@@ -1,13 +1,42 @@
 import javax.management.remote.JMXConnectionNotification;
 import javax.swing.*;
 import java.awt.*;
-public class Gui extends JFrame {
+import java.util.Scanner;
+import java.awt.event.*;
+public class Gui extends JFrame implements ActionListener, MouseListener {
+public void mouseClicked(MouseEvent e) {System.out.println("mouse clicked");
+    System.out.println(e.getX());
+    System.out.println(e.getY());
+    int mousecordsy = e.getY() / 20;
+    int mousecordsx = e.getX() / 20;
+    GamePanel.cordsx = mousecordsx;
+    GamePanel.cordsy = mousecordsy;
+    gamePanel.setCell(gamePanel.gridCells);
+}
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+    }
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+    }
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+    }
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+    }
+
+    public GamePanel gamePanel;
+
+Scanner scan = new Scanner(System.in);
 JMenuBar menuBar;
 JMenu menu;
 JMenuItem menuItemNew;
     public Gui() {
     setTitle("Conways Game Of Life");
-
+    this.gamePanel = new GamePanel();
+    this.add(gamePanel);
     this.getContentPane().setPreferredSize(new Dimension(1200,700));
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -15,14 +44,15 @@ JMenuItem menuItemNew;
     menuBar = new JMenuBar();
     this.setJMenuBar(menuBar);
 
-    GamePanel gamePanel = new GamePanel();
     this.add(gamePanel);
 
     menu = new JMenu("Game");
     menuBar.add(menu);
 
-    menuItemNew = new JMenuItem("new game");
+    menuItemNew = new JMenuItem("New Game");
     menu.add(menuItemNew);
+
+    gamePanel.addMouseListener(this);
 
     this.pack();
     this.toFront();
@@ -30,8 +60,12 @@ JMenuItem menuItemNew;
     this.setVisible(true);
     System.out.println("Gui booted");
 }
-public static void main(String[] args) {
-        new Gui();
+    public static void main(String[] args) {
+        Gui gui = new Gui();
+    }
 
-}
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
 }
