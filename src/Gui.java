5cac4d11c1948b460a7f81e1432d104790941a37
bix <henrybradley2009@gmail.com>
2026-07-21 09:21:48 +1200

    ion even know what I added

--- src/Gui.java
+++ src/Gui.java
@@ -1,35 +1,43 @@
 import javax.management.remote.JMXConnectionNotification;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.awt.event.*;
 public class Gui extends JFrame implements ActionListener, MouseListener {
-public void mouseClicked(MouseEvent e) {
-    if (GamePanel.drawable == true) {
-        System.out.println("click!");
-        System.out.println(e.getX());
-        System.out.println(e.getY());
-        int mousecordsy = e.getY() / 20;
-        int mousecordsx = e.getX() / 20;
-        System.out.println("math done");
-        System.out.println(mousecordsy);
-        System.out.println(mousecordsx);
-        GamePanel.cordsx = mousecordsx;
-        GamePanel.cordsy = mousecordsy;
-        gamePanel.setCell(gamePanel.gridCells);
+    public void mouseClicked(MouseEvent e) {
+        if (GamePanel.drawable == true) {
+            System.out.println("click!");
+
+            System.out.println(e.getX());
+            System.out.println(e.getY());
+
+            int mousecordsy = e.getY() / 20;
+            int mousecordsx = e.getX() / 20;
+
+            System.out.println("math done");
+            System.out.println(mousecordsy);
+            System.out.println(mousecordsx);
+
+            GamePanel.cordsx = mousecordsx;
+            GamePanel.cordsy = mousecordsy;
+            gamePanel.setCell(gamePanel.gridCells);
+        } else if(GamePanel.drawable == false) {
+            System.out.println("Draw false");
+
+        }
     }
-}
 
     @Override
     public void mousePressed(MouseEvent e) {
     }
+
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
     }
+
     @Override
     public void mouseEntered(MouseEvent mouseEvent) {
     }
+
     @Override
     public void mouseExited(MouseEvent mouseEvent) {
     }
@@ -37,58 +45,61 @@ public void mouseClicked(MouseEvent e) {
     public GamePanel gamePanel;
 
 
-JMenuBar menuBar;
-JMenu menu;
-JMenuItem menuItemNew;
-JPanel buttonpanel;
-JMenuItem menuItemStart;
-JMenuItem menuItemStop;
-JButton draw;
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem menuItemNew;
+    JPanel buttonpanel;
+    JMenuItem menuItemStart;
+    JMenuItem menuItemStop;
+    JButton drawbutton;
+
     public Gui() {
-    setTitle("Conways Game Of Life");
-    this.gamePanel = new GamePanel();
-    this.add(gamePanel);
-    this.getContentPane().setPreferredSize(new Dimension(1200,800));
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Conways Game Of Life");
+        this.gamePanel = new GamePanel();
+        this.add(gamePanel);
+        this.getContentPane().setPreferredSize(new Dimension(1200, 800));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
-    menuBar = new JMenuBar();
-    this.setJMenuBar(menuBar);
-
-    this.add(gamePanel);
-
-    menu = new JMenu("Game");
-    menuBar.add(menu);
-
-    menuItemStart = new JMenuItem("Start");
-    menu.add(menuItemStart);
-    menuItemStart.addActionListener(this);
-    menuItemStop = new JMenuItem("Stop");
-    menu.add(menuItemStop);
-    menuItemStop.addActionListener(this);
-    menuItemNew = new JMenuItem("New Game");
-    menu.add(menuItemNew);
-    menuItemNew.addActionListener(this);
-
-    gamePanel.addMouseListener(this);
-
-    buttonpanel = new JPanel();
-    buttonpanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 50));
+        menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
 
 
-    buttonpanel.setBackground(Color.gray);
 
-    draw = new JButton("Draw");
-    draw.addActionListener(this);
-    buttonpanel.add(draw);
+        menu = new JMenu("Game");
+        menuBar.add(menu);
 
-    this.add(buttonpanel, BorderLayout.SOUTH);
-    this.pack();
-    this.toFront();
-    this.pack();
-    this.setVisible(true);
-    System.out.println("Gui booted");
-}
+        menuItemStart = new JMenuItem("Start");
+        menu.add(menuItemStart);
+        menuItemStart.addActionListener(this);
+        menuItemStop = new JMenuItem("Stop");
+        menu.add(menuItemStop);
+        menuItemStop.addActionListener(this);
+        menuItemNew = new JMenuItem("New Game");
+        menu.add(menuItemNew);
+        menuItemNew.addActionListener(this);
+
+        gamePanel.addMouseListener(this);
+
+        buttonpanel = new JPanel();
+        buttonpanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 37));
+
+
+        buttonpanel.setBackground(Color.gray);
+
+        drawbutton = new JButton("Draw");
+        drawbutton.addActionListener(this);
+        buttonpanel.add(drawbutton);
+
+        this.add(buttonpanel, BorderLayout.SOUTH);
+        this.pack();
+        this.toFront();
+        this.pack();
+        gameLogic.start(gamePanel);
+        this.setVisible(true);
+        System.out.println("Gui booted");
+    }
+
     public static void main(String[] args) {
         Gui gui = new Gui();
     }
@@ -105,14 +116,21 @@ JButton draw;
                     GamePanel.drawable = true;
                     System.out.println("Draw true");
                 }
-                    break;
-                    case "Start":
-                        gameLogic.gamerunning = true;
-                    break;
-                    case "Stop":
-                        gameLogic.gamerunning = false;
-                    break;
-
+                break;
+            case "Start":
+                gameLogic.gamerunning = true;
+                GamePanel.drawable = false;
+                System.out.println("True running");
+                break;
+            case "Stop":
+                gameLogic.gamerunning = false;
+                GamePanel.drawable = true;
+                System.out.println("False running");
+                break;
+            case "New Game":
+                System.out.println("test");
+            break;
         }
     }
+
 }
--- src/gameLogic.java
+++ src/gameLogic.java
@@ -1,3 +1,30 @@
+import javax.swing.Timer;
+import javax.swing.*;
+
+
 public class gameLogic {
-public static boolean gamerunning = false;
+    public static int speedms = 200;
+    public static boolean gamerunning = false;
+    private static Timer timer;
+
+    public static int countcells(boolean[][] grid, int row, int col) {
+        int rows = GamePanel.rows;
+        int cols = GamePanel.cols;
+        int count = 0;
+        System.out.println("You are counting");
+
+        return rows;
+    }
+
+    public static void start(GamePanel panel) {
+        timer = new Timer(speedms,
+                actionEvent -> {
+                    for (int row = 0; row < GamePanel.rows; row++) {
+                        for (int col = 0; col < GamePanel.cols; col++) {
+
+
+                        }
+                    }
+                });
+    }
 }
